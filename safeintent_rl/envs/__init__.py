2f8411bd9a7eae7f475ca85fcd585326e208b8c7
kingofrichnight <kingofrichnight@gmail.com>
2026-09-03 21:34:41 -0400

    Export route-progress reward wrapper

--- safeintent_rl/envs/__init__.py
+++ safeintent_rl/envs/__init__.py
@@ -1,5 +1,11 @@
 from safeintent_rl.envs.driver_behavior import DriverBehaviorWrapper, DriverProfile
 from safeintent_rl.envs.intersection import make_intersection_env
+from safeintent_rl.envs.reward import RouteProgressRewardWrapper
 
-__all__ = ["DriverBehaviorWrapper", "DriverProfile", "make_intersection_env"]
+__all__ = [
+    "DriverBehaviorWrapper",
+    "DriverProfile",
+    "RouteProgressRewardWrapper",
+    "make_intersection_env",
+]
 
